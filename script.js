29c675b7bd177a7835bb47a6bac13b78db7d2507
Rubén <rubenncc96@gmail.com>
2024-09-25 19:26:40 +0200

    odio git revert

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// Function to generate a random color in hex format
+
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -11,40 +11,24 @@ function getRandomColor() {
 
 let clickCount = 0;
 let alertCount = 0;
-const aguedaArray = ["bonita", "albacora", "apolínea", "atún", "coquetona",
-                    "cuca", "guapa", "hermosa", "linda", "mona", "preciosa",
-                    "una querubina"
-]
-
-function getRandomWord(){
-    const randomIndex = Math.floor(Math.random() * aguedaArray.length);
-    return aguedaArray[randomIndex]
-}
 
 document.getElementById("changeTextButton").addEventListener("click", function() {
     clickCount++;
-    const randomWord = getRandomWord();
     if (clickCount%10 === 0){
-        // alertCount++;
-        // if (alertCount === 1){
-        //     alert("Caíste en mi trampa, ahora tengo tus datos bancarios");
-        // } 
-        // if (alertCount === 2){
-        //     alert("Nada, es broma, tranqui sigue haciendo el tonto.");
-        // }
-        // if (alertCount > 2){
-        //     alert("También podrías parar e ir a hacer algo productivo...");
-        // }
-        alert(`Oye, disculpa un momento, creo que no te has enterado que Agueda es ${randomWord}`);
-
+        alertCount++;
+        if (alertCount === 1){
+            alert("Caíste en mi trampa, ahora tengo tus datos bancarios");
+        } 
+        if (alertCount === 2){
+            alert("Nada, es broma, tranqui sigue haciendo el tonto.");
+        }
+        if (alertCount > 2){
+            alert("También podrías parar e ir a hacer algo productivo...");
+        }
 
     } else {
-        const randomColor = getRandomColor(); // Generate a random color
-        document.body.style.backgroundColor = randomColor; // Apply the random color to the background
-
-        const texto = document.getElementById("texto");
-        texto.textContent = `Agueda es ${randomWord}`
-
+        const randomColor = getRandomColor();
+        document.body.style.backgroundColor = randomColor; 
 
     }
         
